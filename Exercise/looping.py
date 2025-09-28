74b9e8c6704ade1edadec58c2dffa8e135054f1a
minggo-commits <210210501033@student.unm.ac.id>
2025-09-28 12:08:03 +0700

    Changes to be committed: 	modified:   Exercise/looping.py 	new file:   README.MD

--- Exercise/looping.py
+++ Exercise/looping.py
@@ -2,3 +2,11 @@
 1. print number 1 to 20,
 2. Skip number which divisible by 3,
 3. Stop loop if number was more than 15. """
+
+for i in range(1, 21):
+    if i % 3 == 0:
+        continue
+    if i >= 15:
+        break
+    print(i)
+    
